87eb60086aaf27733e1c1d458c3c890ca69f50bf
Maria Kennedy <maria.kennedy@flatironschool.com>
2018-08-08 21:41:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ Function sayHiToGrandma(){
   return 
   string.toUpperCase()
 }
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
